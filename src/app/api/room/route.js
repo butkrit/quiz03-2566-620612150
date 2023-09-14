034def3806439ce3fc0d3fe59910eb47b1658083
butkrit <yokart.butter@gmail.com>
2023-09-14 15:10:06 +0700

    done

--- src/app/api/room/route.js
+++ src/app/api/room/route.js
@@ -1,5 +1,6 @@
 import { DB, readDB, writeDB } from "@/app/libs/DB";
 import { checkToken } from "@/app/libs/checkToken";
+import { head } from "lodash";
 import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
 
@@ -7,40 +8,50 @@ export const GET = async () => {
   readDB();
   return NextResponse.json({
     ok: true,
-    //rooms:
-    //totalRooms:
+    rooms: DB.rooms,  
+    totalRooms: DB.rooms.length
   });
 };
 
 export const POST = async (request) => {
-  const payload = checkToken();
+  
+  const rawAuthHeader = headers().get("authorization");
+  const token = rawAuthHeader.split(" ")[1];
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
-
+  let role = null;
+  
+  try {
+    const payload = checkToken();
+    
+    role = payload.role;
+  } catch (error) {
+    return NextResponse.json(
+    {
+     ok: false,
+     message: "Invalid token",
+    },
+    { status: 401 }
+    );     
+  }
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room ${"replace this with room name"} already exists`,
-  //   },
-  //   { status: 400 }
-  // );
-
+  
+  if (role === "ADMIN" || role === "SUPER_ADMIN"){
+   return NextResponse.json(
+   {
+     ok: false,
+     message: `Room ${DB.rooms.roomnName} already exists`,
+   },
+   { status: 400 }
+  );
+  }
   const roomId = nanoid();
 
   //call writeDB after modifying Database
   writeDB();
-
+  
   return NextResponse.json({
     ok: true,
-    //roomId,
+    roomId: DB.roomId,
     message: `Room ${"replace this with room name"} has been created`,
   });
 };
--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -4,17 +4,28 @@ import { DB, readDB } from "@/app/libs/DB";
 import { NextResponse } from "next/server";
 
 export const POST = async (request) => {
+  
   readDB();
+  
+  const { username, password } = DB.users;
+  const user = DB.users.find(
+    (user) => user.username === username && user.password === password
+  );
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or Password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
-
-  const token = "Replace this with token creation";
-
+  if ( !user ){
+   return NextResponse.json(
+     {
+       ok: false,
+       message: "Username or Password is incorrect",
+     },
+     { status: 400 }
+   );
+  }else{
+  const token =  jwt.sign(
+    { username, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "8h" }
+  );
   return NextResponse.json({ ok: true, token });
+  }
 };
